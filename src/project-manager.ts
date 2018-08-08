9f860e97db3c791ab1a901cd5ee4929ba0aa5ea1
Julia Zashchitina <jzashch@amazon.com>
2018-08-08 16:25:11 +0000

    Remove file from openFiles on close

--- src/project-manager.ts
+++ src/project-manager.ts
@@ -1075,6 +1075,7 @@ export class ProjectManager implements Disposable {
         if (!config) {
             return
         }
+        config.openFiles.delete(uri)
         config.ensureConfigFile(span)
         config.getHost().incProjectVersion()
     }
